6e179d87feb85592877e10380f1507d4e581a543
mishuagopian <mishuagopian@gmail.com>
2016-06-12 13:12:56 -0300

    Fixing selection methods

--- Equipment/src/utils/SelectionMethod.java
+++ Equipment/src/utils/SelectionMethod.java
@@ -12,95 +12,115 @@ public enum SelectionMethod {
 	Roulette,
 	Universal,
 	Boltzman,
-	Tournament,
+	ProbabilisticTournament,
 	Ranking;
 	
 	public List<Individual> getSelected(List<Individual> population, int n) {
 		List<Individual> selection = new ArrayList<Individual>();
+
 		switch(this) {
+
 		case Elitte:
 			selection.addAll(population);
 			Collections.sort(selection);
 			selection = selection.subList(0, n);
+
 			break;
+
 		case Random:
-			for(int i = 0; i < n; i++) {
+			for (int i = 0; i < n; i++) {
 				int index = (int) Math.random() * population.size();
-				selection.add(population.get(index));
+				selection.add(population.get(index).clone());
 			}
+
 			break;
+
 		case Roulette:
 			GeneticAlgorithmUtils.setAccumulatedAptitude(population);
-			for(int i = 0; i < n; i++) {
+
+			for (int i = 0; i < n; i++) {
 				double rand = Math.random();
 				boolean found = false;
-				for(int j = 0; j < population.size() && !found; j++) {
+
+				for (int j = 0; j < population.size() && !found; j++) {
 					Individual ind = population.get(j);
-					if(ind.getAccumulatedAptitude() > rand) {
-						selection.add(ind);
+					if (ind.getAccumulatedAptitude() > rand) {
+						selection.add(ind.clone());
 						found = true;
 					}
 				}
-				
 			}
+
 			break;
+
 		case Universal:
 			GeneticAlgorithmUtils.setAccumulatedAptitude(population);
 			double rand = Math.random();
-			for(int i = 1; i <= n; i++) {
+
+			for (int i = 1; i < n; i++) {
 				boolean found = false;
 				double randi = (rand + i - 1) / n;
-				for(int j = 0; j < population.size() && !found; j++) {
+
+				for (int j = 0; j < population.size() && !found; j++) {
 					Individual ind = population.get(j);
-					if(ind.getAccumulatedAptitude() > randi) {
-						selection.add(ind);
+					if (ind.getAccumulatedAptitude() > randi) {
+						selection.add(ind.clone());
 						found = true;
 					}
 				}
 				
 			}
+
 			break;
+
 		case Boltzman:
+
 			break;
-		case Tournament:
-			int k = (int) Math.random() * n;
-			for (int i = 0; i < k; i++) {
+
+		case ProbabilisticTournament:
+
+			for (int i = 0; i < n; i++) {
 				double decisionRand = Math.random();
 				Individual ind1 = population.get((int) Math.random() * population.size());
 				Individual ind2 = population.get((int) Math.random() * population.size());
-				if (decisionRand > 0.75) {
+
+				if (decisionRand < 0.75) {
 					if (ind1.compareTo(ind2) > 0) {
-						selection.add(ind1);
+						selection.add(ind1.clone());
 					} else {
-						selection.add(ind1);
+						selection.add(ind2.clone());
 					}
 				} else {
 					if (ind1.compareTo(ind2) > 0) {
-						selection.add(ind1);
+						selection.add(ind2.clone());
 					} else {
-						selection.add(ind1);
+						selection.add(ind1.clone());
 					}
 				}				
 			}
-			for (int i = k; i < n; i++) {
-				selection.add(population.get((int) Math.random() * population.size()));
-			}
+
 			break;
+
 		case Ranking:
 			GeneticAlgorithmUtils.setRankingAptitude(population);
-			for(int i = 1; i <= n; i++) {
+
+			for (int i = 1; i < n; i++) {
 				double rankingRand = Math.random();
 				boolean found = false;
-				for(int j = 0; j < population.size() && !found; j++) {
+
+				for (int j = 0; j < population.size() && !found; j++) {
 					Individual ind = population.get(j);
-					if(ind.getAccumulatedAptitude() > rankingRand) {
-						selection.add(ind);
+					if (ind.getAccumulatedAptitude() > rankingRand) {
+						selection.add(ind.clone());
 						found = true;
 					}
 				}
 			}
+
 			break;
-		default: break;
+
+		default:
+			break;
 		}
 		return selection;
 		
