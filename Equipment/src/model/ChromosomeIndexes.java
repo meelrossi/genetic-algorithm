b5354e2e908231168583a084d77c47a6a8fea0fe
mishuagopian <mishuagopian@gmail.com>
2016-06-12 01:10:43 -0300

    Using everywhere chromosome indexes constants

---
+++ Equipment/src/model/ChromosomeIndexes.java
@@ -0,0 +1,19 @@
+package model;
+
+public enum ChromosomeIndexes {
+	WEAPONS (0),
+	GLOVES (1),
+	SHIRT (2),
+	HELMET (3),
+	BOOTS (4),
+	HEIGHT (5);
+
+	private final int index;
+	ChromosomeIndexes(int index) {
+		this.index = index;
+	}
+	
+	int index() {
+		return this.index;
+	}
+}
